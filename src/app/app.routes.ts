07464545b3d992c791fd74ee372ab98c1020eea2
prasanna <prasannahegde245@gmail.com>
2024-09-08 17:50:49 +0530

    added wishlist component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
 import { HomeComponent } from './home/home.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
 
 //Mention correct urls for routing of the pages
 export const routes: Routes = [
     {path:'',component:HomeComponent,title:"BookBarn"},
-    {path:'cart',component:CartComponent,title:"Cart"}
-
+    {path:'cart',component:CartComponent,title:"Cart"},
+    {path:'wishlist',component: WishlistComponent,title:"Wishlist"}
 ];
